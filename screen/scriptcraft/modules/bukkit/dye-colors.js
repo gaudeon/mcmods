fc3b7e7bfa4e7d49d1f5657288aa5371db88052c
Travis Chase <gaudeon@cpan.org>
2016-01-01 14:25:20 -0700

    added a dye-colors modules for bukkit wool dye colors

---
+++ screen/scriptcraft/modules/bukkit/dye-colors.js
@@ -0,0 +1,37 @@
+var bkWhite     = Packages.org.bukkit.DyeColor.WHITE;
+var bkOrange    = Packages.org.bukkit.DyeColor.ORANGE;
+var bkMagenta   = Packages.org.bukkit.DyeColor.MAGENTA;
+var bkLightBlue = Packages.org.bukkit.DyeColor.LIGHT_BLUE;
+var bkYellow    = Packages.org.bukkit.DyeColor.YELLOW;
+var bkLime      = Packages.org.bukkit.DyeColor.LIME;
+var bkPink      = Packages.org.bukkit.DyeColor.PINK;
+var bkGray      = Packages.org.bukkit.DyeColor.GRAY;
+var bkLightGray = Packages.org.bukkit.DyeColor.SILVER;
+var bkCyan      = Packages.org.bukkit.DyeColor.CYAN;
+var bkPurple    = Packages.org.bukkit.DyeColor.PURPLE;
+var bkBlue      = Packages.org.bukkit.DyeColor.BLUE;
+var bkBrown     = Packages.org.bukkit.DyeColor.BROWN;
+var bkGreen     = Packages.org.bukkit.DyeColor.GREEN;
+var bkRed       = Packages.org.bukkit.DyeColor.RED;
+var bkBlack     = Packages.org.bukkit.DyeColor.Black;
+
+var dye_colors = {
+  white: bkWhite,
+  orange: bkOrange,
+  magenta: bkMagenta,
+  lightblue: bkLightBlue,
+  yellow: bkYellow,
+  lime: bkLime,
+  pink: bkPink,
+  gray: bkGray,
+  lightgray: bkLightGray,
+  cyan: bkCyan,
+  purple: bkPurple,
+  blue: bkBlue,
+  brown: bkBrown,
+  green: bkGreen,
+  red: bkRed,
+  black: bkBlack
+};
+
+module.exports = dye_colors;
