cb958421cbbdb1ca9e0d19299938a5497f6129d1
gaudeon <gaudeon@cpan.org>
2016-01-10 08:24:29 +0000

    Created wool_color module to track bin and rgb values of wool loading jpeg-js in server server will now grab an image from the camera, decode the jpeg with jpeg-js and determine closest color of wool to the pixel pixel list is returned in response to a get call for path /

---
+++ screen/picserve/lib/wool_color.js
@@ -0,0 +1,130 @@
+module.exports = [
+    {
+        name: 'white',
+        color: {
+            r: 221,
+            g: 221,
+            b: 221
+        }
+    },
+    {
+        name: 'orange',
+        color: {
+            r: 219,
+            g: 125,
+            b: 62 
+        }
+    },
+    {
+        name: 'magenta',
+        color: {
+            r: 179,
+            g: 80,
+            b: 188 
+        }
+    },
+    {
+        name: 'light_blue',
+        color: {
+            r: 107,
+            g: 138,
+            b: 201 
+        }
+    },
+    {
+        name: 'yellow',
+        color: {
+            r: 177,
+            g: 166,
+            b: 39 
+        }
+    },
+    {
+        name: 'lime',
+        color: {
+            r: 65,
+            g: 174,
+            b: 56 
+        }
+    },
+    {
+        name: 'pink',
+        color: {
+            r: 208,
+            g: 132,
+            b: 153 
+        }
+    },
+    {
+        name: 'gray',
+        color: {
+            r: 64,
+            g: 64,
+            b: 64 
+        }
+    },
+    {
+        name: 'silver',
+        color: {
+            r: 154,
+            g: 161,
+            b: 161 
+        }
+    },
+    {
+        name: 'cyan',
+        color: {
+            r: 46,
+            g: 110,
+            b: 137 
+        }
+    },
+    {
+        name: 'purple',
+        color: {
+            r: 126,
+            g: 61,
+            b: 181 
+        }
+    },
+    {
+        name: 'blue',
+        color: {
+            r: 46,
+            g: 56,
+            b: 141 
+        }
+    },
+    {
+        name: 'brown',
+        color: {
+            r: 79,
+            g: 50,
+            b: 31 
+        }
+    },
+    {
+        name: 'green',
+        color: {
+            r: 53,
+            g: 70,
+            b: 27 
+        }
+    },
+    {
+        name: 'red',
+        color: {
+            r: 150,
+            g: 52,
+            b: 48 
+        }
+    },
+    {
+        name: 'black',
+        color: {
+            r: 25,
+            g: 22,
+            b: 22 
+        }
+    }
+];
--- screen/picserve/server.js
+++ screen/picserve/server.js
@@ -1,7 +1,11 @@
 'use strict';
 
-var express    = require('express'),
-    body_parser = require('body-parser');
+var express     = require('express'),
+    body_parser = require('body-parser'),
+    jpeg        = require('jpeg-js'),
+    exec        = require('child_process').exec,
+    fs          = require('fs'),
+    wool_color  = require('./lib/wool_color');
 
 // Globals
 var PORT = 8080;
@@ -14,7 +18,10 @@ server.use(body_parser.json());
 server.use(body_parser.urlencoded({ extended: true}));
 
 server.get('/', function(req, res) {
-    res.json({
+    get_pic(function(pixels) {
+        res.json({
+            'pixels': pixels
+        });
     });
 });
 
@@ -22,3 +29,61 @@ server.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
 
 /* --- FUNCTIONS --- */
+function get_pic(callback) {
+
+    if ('function' !== typeof callback) callback = function() {};
+
+    var camcmd   = 'raspistill';
+    var width    = 64;
+    var height   = 48;
+    var img_file = __dirname + '/.picam.jpg';
+
+    // e.g. raspistill -w 64 -h 48 -o filename.jpg
+    var cmd = camcmd
+                + " -w " + width
+                + " -h " + height
+                + " -o " + img_file;
+
+
+    exec(cmd, function(error, stdout, stderr) {
+        var file   = fs.readFileSync(img_file);
+        var raw    = jpeg.decode(file);
+        var pixels =  [];
+
+        // each pixel is rgba 
+        for (var i = 0; i < raw.width * raw.height * 4; i += 4) {
+            var r = raw.data[i],
+                g = raw.data[i+1],
+                b = raw.data[i+2],
+                a = raw.data[i+3];
+
+            pixels.push(closest_color(r,g,b)); 
+        }
+
+        callback(pixels);
+    }); 
+}
+
+var COLOR_CACHE = {};
+function closest_color(r, g, b) {
+    var cache_id = r + ':' + g + ':' + b;
+
+    if ('undefined' !== typeof COLOR_CACHE[cache_id]) {
+        return COLOR_CACHE[cache_id];
+    }
+
+    var color = -1,
+        match = 1000;
+
+    for (var c = 0; c < wool_color.length; c++) {
+        var cur_match = Math.abs(r - wool_color[c].color.r) + Math.abs(g - wool_color[c].color.g) + Math.abs(b - wool_color[c].color.b);
+
+        if(cur_match < match) {
+            match = cur_match;
+            color = c;
+        }
+    }
+
+    COLOR_CACHE[cache_id] = color;
+    return color;
+}
